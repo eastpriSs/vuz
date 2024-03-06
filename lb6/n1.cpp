98ba7eb43da89ffd94ac0adb791de315673776e4
eastpriSs <73599984+eastpriSs@users.noreply.github.com>
2024-03-07 01:58:11 +0700

    Add files via upload

---
+++ lb6/n1.cpp
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define AMOUNT_NUMBERS 20 // кратно 20
+
+void fillOriginFile(FILE * pf)
+{
+    int posNumber = rand() % 100 + 1;
+    int negNumber = -1 * (rand() % 100 + 1);
+    for (int i = 0; i <= (int)(AMOUNT_NUMBERS / 20) ; ++i)
+    {   
+        for (int i_p = 0; i_p < 5; ++i_p, 
+            posNumber = rand() % 100 + 1)
+            fprintf(pf, "%d ", posNumber);
+        
+        for (int i_n = 0; i_n < 5; ++i_n, 
+            negNumber = -1 * (rand() % 100 + 1) )
+            fprintf(pf, "%d ", negNumber);
+    }
+}
+
+void outputDataFile(FILE * pf)
+{
+    int numberfFile;
+    for (int i = 0; i < AMOUNT_NUMBERS; ++i)
+    {
+        fscanf(pf, "%d", &numberfFile);
+        printf("%d ", numberfFile);
+    }
+    
+}
+
+void fillResultFile(FILE * in, FILE * from)
+{
+    int fromfData [AMOUNT_NUMBERS] = {0};
+    int fromfNumber;
+    int id_pos = 0, id_neg = AMOUNT_NUMBERS / 2;
+    
+    for (int i = 0; i < AMOUNT_NUMBERS; ++i) {
+        fscanf(from, "%d", &fromfNumber);
+        if (fromfNumber > 0)
+            fromfData[id_pos++] = fromfNumber;
+        else
+            fromfData[id_neg++] = fromfNumber; 
+    }
+
+    int i_p = 0, i_n = AMOUNT_NUMBERS/2;
+    for (int i = 2; i <= (int)(AMOUNT_NUMBERS / 2); i += 2)
+    {
+        for (; i_p < i; ++i_p)
+            fprintf(in, "%d ", fromfData[i_p]); 
+
+        for (; i_n < AMOUNT_NUMBERS/2 + i; ++i_n)
+            fprintf(in, "%d ", fromfData[i_n]); 
+
+    }
+    
+}
+
+int main()
+{
+    FILE * ptrFile     = fopen("origin.txt", "w");
+    FILE * ptrOutFile  = fopen("result.txt", "w");
+    
+    printf("\nOrigin file:");
+    fillOriginFile(ptrFile);
+    fclose(ptrFile); // Записывем в файл
+    ptrFile = fopen("origin.txt", "r");
+    outputDataFile(ptrFile);
+    
+    printf("\n");
+    fseek(ptrFile, 0, SEEK_SET); // сброс указателя
+
+    printf("\nResult file:");    
+    fillResultFile(ptrOutFile, ptrFile);
+    fclose(ptrOutFile); // Записывем в файл
+    ptrOutFile = fopen("result.txt", "r");
+    outputDataFile(ptrOutFile);
+    
+    printf("\n");
+}
---
+++ lb6/n2.cpp
@@ -0,0 +1,105 @@
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_TEXT_SIZE 255
+#define TEXT "q a b a b v d"
+
+#define SEPARATORS " -,/.!?():;\'\""
+
+void fillFile(FILE * f)
+{
+    fputs(TEXT, f);
+}
+
+void outputDataFile(FILE * f)
+{
+    char chff;
+    while ( (chff = fgetc(f)) != EOF )
+        putchar(chff);
+}
+
+char findWord(char words[MAX_TEXT_SIZE/2][MAX_TEXT_SIZE], int amountWords, char word [] )
+{
+    for (int i = 0; i < amountWords; ++i)
+        if (stricmp(words[i], word) == 0) return 1;
+    return 0;    
+}
+
+void freadCharsTo(char a[], FILE * f)
+{
+    char chff;
+    int amountCh = 0;
+    while ( (chff = fgetc(f)) != EOF )
+        a[amountCh++] = chff;
+}
+
+void countWordsAndWrite(char words[MAX_TEXT_SIZE/2][MAX_TEXT_SIZE], 
+                            int resultAmount[MAX_TEXT_SIZE/2], int amountLex)
+{
+    for (int i = 0; i < amountLex; ++i) 
+        for (int j = 0; j < amountLex; ++j)
+            if (stricmp(words[i], words[j]) == 0)
+                resultAmount[i] += 1;
+}
+
+int findMax(int a[MAX_TEXT_SIZE/2], int len)
+{
+    int max = a[0];
+    for (int i = 0; i < len; i++)
+        if (a[i] > max) max = a[i];
+    return max;
+}
+
+void outResult(FILE * f)
+{
+    char wordsf [MAX_TEXT_SIZE/2][MAX_TEXT_SIZE];
+    char outputedWordsf [MAX_TEXT_SIZE/2][MAX_TEXT_SIZE]; // место!!
+    char strfF [MAX_TEXT_SIZE];
+    int amountOfWordsInText[MAX_TEXT_SIZE/2] = {0};
+
+    int amountLex = 0;
+
+    // Считывание
+    freadCharsTo(strfF, f);
+
+    // Разбиение на слова
+    char * word = strtok(strfF, SEPARATORS);
+    while (word != NULL)
+    {
+        strcpy(wordsf[amountLex++], word);
+        word = strtok(NULL, SEPARATORS);
+    }
+
+    // Подсчет каждого слова
+    countWordsAndWrite(wordsf, amountOfWordsInText, amountLex);
+    
+    // Нахождение максимального
+    int max = findMax(amountOfWordsInText, amountLex); 
+
+    // Вывод слов, если они упоминаются в тексте максимальное кол-во раз раз
+    int amountOutLex = 0;
+    printf("\nMeet most:");
+    for (int i = 0; i < amountLex; ++i)
+        if (amountOfWordsInText[i] == max 
+                    && !findWord(outputedWordsf, amountOutLex, wordsf[i])) {
+            strcpy(outputedWordsf[amountOutLex++], wordsf[i]);
+            printf("\n%s, amount: %d", wordsf[i], amountOfWordsInText[i]);
+        }
+}
+
+int main()
+{
+    FILE * ptrFileW = fopen("out2.txt", "w");
+    FILE * ptrFileR = fopen("out2.txt", "r");
+    
+    printf("\nOrigin file:");
+    fillFile(ptrFileW);
+    fclose(ptrFileW);
+    outputDataFile(ptrFileR);
+    
+    fseek(ptrFileR, 0, SEEK_SET); // сброс указателя
+
+    printf("\nResult:");
+    outResult(ptrFileR);
+}
